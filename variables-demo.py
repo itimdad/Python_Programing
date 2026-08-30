844ee8c9d0b726a87e589794c4332fb85ed49920
Imdad Firdausi <iamfirdausi01@gmail.com>
2026-08-30 12:25:24 +0530

    Learning data type in python

--- variables-demo.py
+++ variables-demo.py
@@ -19,4 +19,18 @@ print(sys.getsizeof(number))
 #list data type(ordered + mutable + duplicates allowed)
 students = ["Aman", "Ajay", "Karan", "Aman"]
 print(students)
+students[0] = "Vijay"
+print(students)
 print(type(students))
+
+#tuple data type (ordered +immutable + duplicates allowed)
+ids = (2,5,7,9,0,1)
+courses = ("AI", "Python", "Java")
+print(ids)
+print("courses => ", courses)
+print(type(ids))
+
+#set data type (unordered + Mutable + No Duplicates)
+skills = {"Java", "SonarQube", "Maven", "Gradle", "Vunerability"}
+print(skills)
+print(type(skills))
